100c69b593d46be9898df288c5d9c61fc8738077
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-09-28 20:58:34 -0300

    initial commit

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,7 @@
-// configure aqui sua store
+import { legacy_createStore as CreateStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import reducers from './reducers';
+
+const store = CreateStore(reducers, composeWithDevTools());
+
+export default store;
